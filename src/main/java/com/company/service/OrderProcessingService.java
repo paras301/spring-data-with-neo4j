72525017c89bef4d4d87bff48dd045e97753fb8c
Paras Bansal <paras.bansal@ironmountain.com>
2021-06-26 19:31:52 -0400

    Final code

--- src/main/java/com/company/service/OrderProcessingService.java
+++ src/main/java/com/company/service/OrderProcessingService.java
@@ -1,75 +1,65 @@
 package com.company.service;
 
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Map;
 
+import org.neo4j.driver.Driver;
+import org.neo4j.driver.Session;
+import org.neo4j.driver.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.company.neo4j.repo.CustomerRepository;
-import com.company.neo4j.repo.OrderRepository;
-import com.company.neo4j.vo.Customer;
-import com.company.neo4j.vo.Order;
-import com.company.neo4j.vo.OrderItems;
+import com.company.vo.CypherQueries;
 import com.company.vo.OrderCartInput;
 import com.company.vo.OrderItem;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Service
 @Slf4j
 public class OrderProcessingService {
-	
+
+	private final Driver driver;
+
+	public OrderProcessingService(Driver driver) {
+		this.driver = driver;
+	}
+
 	@Autowired
-	CustomerRepository customerRepository;
-	
-	@Autowired
-	OrderRepository orderRepository;
+	CypherQueries cypherQueries;
 
 	public String processOrder(OrderCartInput req) throws Exception {
 		log.info("req --> " + req);
-		
-		//Business Logic Layer to process incoming data
-		
-		Customer c = customerRepository.findCustomerByName(req.getName());
-		
-		log.info("c --> " + c);
-		
-		if (c == null) {
-			c = new Customer(req.getCustomerId(), req.getName());
-		}
-		
-		log.info("c --> " + c);
-		
-		Order oTemp = orderRepository.findOrderById(req.getName(), req.getOrderId());
-		
-		log.info("oTemp --> " + oTemp);
-		
-		if(oTemp == null) {
-			
-			oTemp = new Order(req.getOrderId(), req.getOrderDate());
-			
-			for(OrderItem otTemp : req.getItems()) {
-				oTemp.addOrderItem(new OrderItems(otTemp.getItem(), otTemp.getUnit_price(), otTemp.getQuantity()));
+
+		String createOrder = mapStringToMapping(cypherQueries.getCypher_queries().get("createOrder"), req);
+
+		for (OrderItem item : req.getItems()) {
+			String createOrder1 = mapStringToMapping(createOrder, item);
+
+			try (Session session = driver.session(); Transaction tx = session.beginTransaction()) {
+				tx.run(createOrder1);
+				tx.commit();
+			} catch (Exception e) {
+				log.error("Exception while processing data to Neo4j... ", e);
 			}
-			
-			
-			log.info("oTemp --> " + oTemp);
-			
-			c.placeOrder(oTemp);
-			
-			log.info("c --> " + c);
-			
-			customerRepository.save(c);
-		} else {
-			log.error("Order --> " + req.getOrderId() + " Already processed!");
-			return "Order was already processed";
+
 		}
-		
-		//Business Logic end
-		
+
 		return "Customer Order Processed Successfully";
-		
+
+	}
+
+	private String mapStringToMapping(String query, Object mapping) {
+		ObjectMapper oMapper = new ObjectMapper();
+		Map<String, Object> query_params = oMapper.convertValue(mapping, Map.class);
+
+		for (Map.Entry<String, Object> entry : query_params.entrySet()) {
+			if (query.contains("${" + entry.getKey() + "}"))
+				query = query.replace("${" + entry.getKey() + "}", (String) entry.getValue());
+		}
+
+		log.info("Executing cypher query --> " + query);
+
+		return query;
 	}
 }
--- src/main/java/com/company/vo/OrderItem.java
+++ src/main/java/com/company/vo/OrderItem.java
@@ -1,7 +1,5 @@
 package com.company.vo;
 
-import java.util.List;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
